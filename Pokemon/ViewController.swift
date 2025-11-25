cf3af7d5e7ad2aed0fce918490589be264c062d9
Derrick <bightm54012@gmail.com>
2025-11-25 13:22:53 +0800

    init

---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Sharon Chao on 2025/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
